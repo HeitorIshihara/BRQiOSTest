38917d7d0665deaa7c32787c9fc713afbc1c6f08
Heitor Kenji Takeo Ishihara <heitorkenji@nbbv027838.local>
2019-01-04 13:39:23 -0200

    Adding Xcode project

---
+++ BRQiOSTest/BRQiOSTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BRQiOSTest
+//
+//  Created by Heitor Kenji Takeo Ishihara on 04/01/2019.
+//  Copyright © 2019 Heitor Kenji Takeo Ishihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
