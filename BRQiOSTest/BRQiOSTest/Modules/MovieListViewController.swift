34eb78e57ae72f9f579788e999a3f9cce7a94d34
Heitor Kenji Takeo Ishihara <heitorkenji@nbbv027838.local>
2019-01-04 13:48:25 -0200

    Adding MVVM module for MovieList

--- BRQiOSTest/BRQiOSTest/Modules/MovieListViewController.swift
+++ BRQiOSTest/BRQiOSTest/Modules/MovieListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MovieListViewController.swift
 //  BRQiOSTest
 //
 //  Created by Heitor Kenji Takeo Ishihara on 04/01/2019.
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MovieListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
---
+++ BRQiOSTest/BRQiOSTest/Modules/MovieListViewModal.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListViewModal.swift
+//  BRQiOSTest
+//
+//  Created by Heitor Kenji Takeo Ishihara on 04/01/2019.
+//  Copyright © 2019 Heitor Kenji Takeo Ishihara. All rights reserved.
+//
+
+import Foundation
+
+class MovieListViewModal {
+    
+}
