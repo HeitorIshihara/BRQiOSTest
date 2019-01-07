f48c0024ae50da84a19e0c981badeb2ca32dd763
Heitor Kenji Takeo Ishihara <ishihara.heitor@gmail.com>
2019-01-07 17:15:33 -0200

    Testing if the requests urls are correct

---
+++ BRQiOSTest/BRQiOSTestTests/DecodeTests.swift
@@ -0,0 +1,9 @@
+//
+//  DecodeTests.swift
+//  BRQiOSTestTests
+//
+//  Created by Heitor Kenji Takeo Ishihara on 07/01/2019.
+//  Copyright © 2019 Heitor Kenji Takeo Ishihara. All rights reserved.
+//
+
+import Foundation
