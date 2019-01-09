cb74acd5cac6eb7ede0b9cf92a3a4e16c6b4ebb8
Heitor Kenji Takeo Ishihara <ishihara.heitor@gmail.com>
2019-01-09 10:55:03 -0200

    Adding test for MovieSelected

--- BRQiOSTest/BRQiOSTestTests/Unit Tests/MovieListTests.swift
+++ BRQiOSTest/BRQiOSTestTests/Unit Tests/MovieListTests.swift
@@ -11,14 +11,19 @@ import XCTest
 
 class MovieListTests: XCTestCase {
     
+    var movieListViewController: MovieListViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.movieListViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MovieList") as? MovieListViewController
+        self.movieListViewController?.viewModel.movies = [Movie(id: "t1", title: "Thor"), Movie(id: "t2", title: "Captain America"), Movie(id: "t3", title: "Iron Man")]
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        self.movieListViewController = nil
     }
     
     func testSearchURL() {
@@ -31,12 +36,16 @@ class MovieListTests: XCTestCase {
     }
     
     func testGetMovie() {
-        let viewController: MovieListViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MovieList") as! MovieListViewController
-        viewController.viewModel.movies = [Movie(id: "t1", title: "Thor"), Movie(id: "t2", title: "Captain America"), Movie(id: "t3", title: "Iron Man")]
-        
-        let movie: Movie = viewController.viewModel.getMovie(at: IndexPath(row: 1, section: 0))
+        let movie: Movie = movieListViewController.viewModel.getMovie(at: IndexPath(row: 1, section: 0))
         
         XCTAssertEqual(Movie(id: "t2", title: "Captain America"), movie)
+    }
+    
+    func testMovieSelected() {
+        movieListViewController.viewModel.movieSelected(at: IndexPath(row: 1, section: 0))
+        if let movieSelected: Movie = movieListViewController.viewModel.movieSelected {
+            XCTAssertEqual(Movie(id: "t2", title: "Captain America"), movieSelected)
+        }
         
     }
     
